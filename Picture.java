2069f3432a49d106ab80dedb1adccfe992845ceb
Ngomes1 <nicholas.gomes@raritanval.edu>
2023-01-23 12:43:27 -0500

    Added 2 person objects and added it to the Picture.java file

--- Picture.java
+++ Picture.java
@@ -15,6 +15,9 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Person  bob;
+    private Person jill;
+    private Square  chimney;
     private boolean drawn;
 
     /**
@@ -26,6 +29,9 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        bob = new Person();
+        jill = new Person();
+        chimney = new Square();
         drawn = false;
     }
 
@@ -35,19 +41,25 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
+            wall.moveHorizontal(-180);
             wall.moveVertical(20);
             wall.changeSize(120);
             wall.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
+            window.changeColor("blue");
+            window.moveHorizontal(-160);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
-    
+            
+            chimney.changeColor("red");
+            chimney.moveHorizontal(-110);
+            chimney.moveVertical(-40);
+            chimney.changeSize(30);
+            chimney.makeVisible();
+            
             roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
+            roof.moveHorizontal(-20);
             roof.moveVertical(-60);
             roof.makeVisible();
     
@@ -56,6 +68,21 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            bob.changeColor("blue");
+            bob.moveHorizontal(100);
+            bob.moveVertical(0);
+            bob.changeSize(80,40);
+            bob.makeVisible();
+            
+            jill.changeColor("magenta");
+            jill.moveHorizontal(140);
+            jill.moveVertical(0);
+            jill.changeSize(50,30);
+            jill.makeVisible();
+            
+            
+            
             drawn = true;
         }
     }
@@ -69,6 +96,9 @@ public class Picture
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
+        bob.changeColor("black");
+        jill.changeColor("black");
+        chimney.changeColor("black");
     }
 
     /**
@@ -76,9 +106,12 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        wall.changeColor("green");
+        window.changeColor("blue");
+        roof.changeColor("blue");
+        sun.changeColor("red");
+        bob.changeColor("blue");
+        jill.changeColor("magenta");
+        chimney.changeColor("green");
     }
 }
